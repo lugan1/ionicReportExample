fa7a39963d01853fb66a4c8953d85a4771ae2cec
hglee <hglee@softnet.co.kr>
2021-12-14 19:30:11 +0900

    #1 회원가입 페이지 작업 진행중

--- src/app/page/sign-up/sign-up.page.ts
+++ src/app/page/sign-up/sign-up.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { alertController } from '@ionic/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Observable} from "rxjs";
 import {SignUpService} from "../../service/sign-up.service";
 
@@ -12,25 +12,60 @@ import {SignUpService} from "../../service/sign-up.service";
 export class SignUpPage implements OnInit {
 
   fg_signUp : FormGroup;
+  id_duplicate_valid : boolean;
+  name_duplicate_valid : boolean;
 
-  constructor(private formBuilder:FormBuilder,
-              private signUpService:SignUpService) {
-
-    this.fg_signUp = formBuilder.group({
-      id: [''],
-      pw: [''],
+  constructor(private formBuilder:FormBuilder, private  signUpService:SignUpService) {
+    this.fg_signUp = this.formBuilder.group({
+      id: this.formBuilder.control('', [Validators.required, Validators.minLength(4)]),
+      pw: this.formBuilder.control('',[Validators.pattern("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")]),
       pw_verify: [''],
-      name: ['']
-      })
+      name: this.formBuilder.control('',[Validators.required, Validators.minLength(4)])
+    })
+    //회원가입 formGrup 생성
+
   }
+
 
   ngOnInit() {
+    this.fg_signUp.controls['id'].valueChanges.subscribe(data=>{
+      this.id_duplicate_valid = false;
+    })
+    // 폼컨트롤의 입력값이 바뀌면, ID 중복검사를 다시 해야된다.
+
+    this.fg_signUp.controls['name'].valueChanges.subscribe(data=>{
+      this.name_duplicate_valid = false;
+    })
+    // 폼컨트롤의 입력값이 바뀌면, 닉네임 중복검사를 다시 해야된다.
+  }
+
+  public check_id_exists() : void{
+    this.signUpService.Get_existsId(this.fg_signUp.controls['id'].value).subscribe(data=> {
+      if(data != 0){
+        this.id_duplicate_valid = false;
+        this.fg_signUp.controls['id'].setErrors({"duplicate":true});
+        this.open_duplicateAlert("중복확인", "중복되는 아이디가 존재합니다.");
+      }
+      if(data == 0){
+        this.id_duplicate_valid = true;
+      }
+    })
+  }
+
+  public check_name_exists() : void{
+    this.signUpService.Get_existsName(this.fg_signUp.controls['name'].value).subscribe(data=> {
+      if(data != 0){
+        this.name_duplicate_valid = false;
+        this.fg_signUp.controls['name'].setErrors({"duplicate":true});
+        this.open_duplicateAlert("중복확인", "중복되는 닉네임 존재합니다.");
+      }
+      if(data == 0){
+        this.name_duplicate_valid = true;
+      }
+    })
   }
 
 
-  public id_exists():Observable<any>{
-    return
-  }
 
   //async 익명함수를 open_submitAlert 에 할당한다.
   open_submitAlert = async () => {
@@ -39,13 +74,22 @@ export class SignUpPage implements OnInit {
       message: '정말로 제출하시겠습니까?',
       buttons: [
         {text: '아니오'},
-        {text:'예', handler : ()=>{ console.log(this.fg_signUp) }
+        {text:'예', handler : ()=>{ console.log(this.fg_signUp.controls['id'].invalid) }
         }
         ]
     });
     await alert.present();
     // alert 오브젝트가 생성되고 난 다음에 표시한다.
-  }
+  };
+
+  open_duplicateAlert = async (title:string, content:string) =>{
+    const alert = await alertController.create({
+      header:title,
+      message:content,
+      buttons: [{text: '확인'}]
+    });
+    await alert.present();
+  };
 
 
 }
--- src/app/service/sign-up.service.ts
+++ src/app/service/sign-up.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Request_SignUp} from "../model/request_SignUp";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,20 @@ export class SignUpService {
   constructor(private httpClient:HttpClient) { }
 
 
+  public Post_signUp(signUp:Request_SignUp):Observable<any>{
+    return this.httpClient.post<any>('http://localhost:8087/api/back/user/insertUser',signUp);
+  }
 
+  public Get_existsId(id:string):Observable<any>{
+    let params = new HttpParams().set("id", id);
+
+    return this.httpClient.get<any>('http://localhost:8087/api/back/user/existsId', {params:params});
+  }
+
+  public Get_existsName(name:string):Observable<any>{
+    let params = new HttpParams().set("name", name);
+
+    return this.httpClient.get<any>('http://localhost:8087/api/back/user/existsName', {params:params});
+  }
 
 }
